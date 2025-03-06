d0f97a5254fa6fd4ab8a244e2371e37082365004
Shivansh Garg <Shivanshgarg007@gmail.com>
2025-03-06 15:26:43 +0530

    Deployed Frontend

--- frontend/src/ASAPCard.jsx
+++ frontend/src/ASAPCard.jsx
@@ -1,16 +1,21 @@
 const ASAPCard = () => {
-    return (
-      <div className="flex justify-center items-center min-h-screen bg-gray-100">
-        <div className="bg-white p-6 rounded-2xl shadow-xl text-center max-w-sm flex flex-col items-center">
-          <h1 className="text-3xl font-bold text-gray-900">Shivansh Garg</h1>
-          <h2 className="text-xl text-blue-600 mt-2">ASAP Project</h2>
-          <p className="text-gray-700 mt-4 text-center">
-            A powerful and simple TODO list to help manage tasks efficiently.
-          </p>
-        </div>
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="bg-white p-6 rounded-2xl shadow-xl text-center max-w-sm flex flex-col items-center">
+        <h1 className="text-6xl font-bold text-yellow-400">Shivansh</h1>
+        <h1 className="text-6xl font-bold text-yellow-400">Garg</h1>
+
+        <h2 className="text-xl text-blue-600 mt-2">ASAP Project</h2>
+        <p className="text-gray-700 mt-4 text-center">
+          A powerful and simple TODO list to help manage tasks efficiently.
+        </p>
+
+        <button className="mt-4 px-4 py-2 bg-red-500 text-white font-bold rounded">
+          Tailwind is working
+        </button>
       </div>
-    );
-  };
-  
-  export default ASAPCard;
-  
+    </div>
+  );
+};
+
+export default ASAPCard;
