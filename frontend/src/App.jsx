08a9124da7399cbd5895ccd6f5278ff4148900ec
Shivansh Garg <Shivanshgarg007@gmail.com>
2025-03-17 09:38:04 +0530

    TodoItem added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import "./index.css";
-import ASAPCard from "./ASAPCard";
-
+import ASAPCard from "./components/ASAPCard";
+import TodoItem from "./components/TODOItem";
 
 function App() {
+
+  const dummyTodo = {
+    task: "Complete ASAP Project",
+    status: "pending"
+  };
+
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 space-y-6">
       <ASAPCard />
+      <TodoItem {...dummyTodo} />
     </div>
   );
 }
